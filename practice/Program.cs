d46145aa8a1a553e35b57808424924d7633ac81b
Atitep Anuchettarak <atitep@thaigertec.local>
2017-10-17 15:41:07 +0700

    add testJoin

--- practice/Program.cs
+++ practice/Program.cs
@@ -8,6 +8,19 @@ namespace practice
 {
     class Program
     {
+        class DepartmentClass
+        {
+            public int DepartmentId { get; set; }
+            public string Name { get; set; }
+        }
+
+        class EmployeeClass
+        {
+            public int EmployeeId { get; set; }
+            public string EpmployeeName { get; set; }
+            public int DepartmentId { get; set; }
+        }
+
 
         public static void testFilter()
         {
@@ -33,11 +46,38 @@ namespace practice
             }
             Console.ReadLine();
         }
-        
+
+        public static void testJoin()
+        {
+            List<DepartmentClass> departments = new List<DepartmentClass>();
+            departments.Add(new DepartmentClass { DepartmentId = 1, Name = "Account" });
+            departments.Add(new DepartmentClass { DepartmentId = 2, Name = "Sales" });
+            departments.Add(new DepartmentClass { DepartmentId = 3, Name = "Marketing" });
+
+            List<EmployeeClass> employees = new List<EmployeeClass>();
+            employees.Add(new EmployeeClass { DepartmentId = 1, EmployeeId = 1, EpmployeeName = "William" });
+            employees.Add(new EmployeeClass { DepartmentId = 2, EmployeeId = 2, EpmployeeName = "Miley" });
+            employees.Add(new EmployeeClass { DepartmentId = 1, EmployeeId = 3, EpmployeeName = "Benjamin" });
+            employees.Add(new EmployeeClass { DepartmentId = 3, EmployeeId = 4, EpmployeeName = "William" });
+
+            var list = (from e in employees
+                        join d in departments on e.DepartmentId equals d.DepartmentId
+                        select new { EmployeeName = e.EpmployeeName, DepartmentName = d.Name }
+                );
+
+            foreach (var e in list)
+            {
+                Console.WriteLine("Employee Name = {0} , Department Name = {1}", e.EmployeeName, e.DepartmentName);
+            }
+
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
 
         static void Main(string[] args)
         {
-            testFilter();
+            //testFilter();
+            testJoin();
         }        
     }
 }

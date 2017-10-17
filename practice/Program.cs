433dcaad79572b0d8b38c3e0609c179caee18a31
Atitep Anuchettarak <atitep@thaigertec.local>
2017-10-17 15:38:30 +0700

    add testFilter

---
+++ practice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    class Program
+    {
+
+        public static void testFilter()
+        {
+            List<string> words = new List<string>() { "an", "apple", "a", "day" };
+
+            var query = from word in words select word.Substring(0, 1);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+            List<string> phrases = new List<string>() { "an apple a day", "the quick brown fox" };
+
+            var query2 = from phrase in phrases
+                         from word in phrase.Split(' ')
+                         select word;
+
+            foreach (var item in query2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+        
+
+        static void Main(string[] args)
+        {
+            testFilter();
+        }        
+    }
+}
+

5f32b607e0160c7a8d41c995e98128e59b305871
Atitep Anuchettarak <atitep@thaigertec.local>
2017-10-17 15:51:40 +0700

    add testGroup

--- practice/Program.cs
+++ practice/Program.cs
@@ -100,11 +100,28 @@ namespace practice
             Console.ReadLine();
         }
 
+        public static void testGroup()
+        {
+            List<int> numbers = new List<int>() { 35, 44, 200, 84, 3987, 4, 199, 329, 446, 208 };
+            IEnumerable<IGrouping<int, int>> query = from number in numbers
+                                                     group number by number % 2;
+
+            foreach (var group in query)
+            {
+                Console.WriteLine(group.Key == 0 ? "\nEven numbers:" : "\nOdd numbers:");
+
+                foreach (int i in group)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             //testFilter();
             //testJoin();
-            testOrderBy();
+            //testOrderBy();
+            testGroup();
         }        
     }
 }
